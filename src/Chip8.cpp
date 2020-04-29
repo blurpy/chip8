5dda27d6895d17b82e58275443b5b13a4dea040a
Christian Ihle <blurpy@gmail.com>
2020-04-29 18:55:09 +0200

    Add registers and memory fields

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -8,11 +8,8 @@
 #include "Chip8.h"
 
 static const int ROM_OFFSET = 512;
-static const int MEMORY_SIZE = 4096;
 
 void Chip8::run(const std::string &fileName) {
-    std::vector<uint8_t> memory(MEMORY_SIZE);
-
     const auto window = std::make_unique<Window>("CHIP-8", 640, 480);
     const auto romLoader = std::make_unique<RomLoader>(fileName);
     const auto debugger = std::make_unique<Debugger>();
--- src/Chip8.h
+++ src/Chip8.h
@@ -2,11 +2,21 @@
 #define CHIP8_CHIP8_H
 
 #include <string>
+#include <vector>
+
+static const int MEMORY_SIZE = 4096;
 
 class Chip8 {
 
 public:
     void run(const std::string &fileName);
+
+private:
+    std::vector<uint8_t> memory = std::vector<uint8_t>(MEMORY_SIZE); // 4096 8-bit memory locations
+
+    uint8_t V[16]{}; // V0 to VF 8-bit registers
+    uint16_t I = 0;  // 16 bit I-register
+    uint16_t PC = 0; // 16-bit Program counter
 };
 
 #endif //CHIP8_CHIP8_H
